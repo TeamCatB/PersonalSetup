e165a4b0a32aac29f527b0409321da98fdc7c908
G <mortoxia@gmail.com>
2025-03-03 23:14:35 -0600

    add extra channel

--- Guix/channels.scm
+++ Guix/channels.scm
@@ -7,5 +7,27 @@
         "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
         (openpgp-fingerprint
           "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB B2D5"))))
+  (channel
+    (name 'gchannel)
+    (url "https://github.com/GigiaJ/Guix-Personal-Packages.git")
+    (branch "main"))
+(channel
+  (name 'efraim-dfsg)
+  (url "https://git.sr.ht/~efraim/my-guix")
+  (branch "master")
+  (introduction
+    (make-channel-introduction
+      "61c9f87404fcb97e20477ec379b643099e45f1db"
+      (openpgp-fingerprint
+        "A28B F40C 3E55 1372 662D  14F7 41AA E7DC CA3D 8351"))))
+    (channel
+    (name 'saayix)
+    (branch "main")
+    (url "https://codeberg.org/look/saayix")
+    (introduction
+      (make-channel-introduction
+        "12540f593092e9a177eb8a974a57bb4892327752"
+        (openpgp-fingerprint
+          "3FFA 7335 973E 0A49 47FC  0A8C 38D5 96BE 07D3 34AB"))))
   %default-channels)
 
