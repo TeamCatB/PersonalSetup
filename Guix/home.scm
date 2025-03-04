ab84103fb0c2dd5e3bcd6743472b16fbe3b5c25f
G <mortoxia@gmail.com>
2025-03-03 23:14:52 -0600

    update home to be current

--- Guix/home.scm
+++ Guix/home.scm
@@ -1,21 +1,90 @@
 ;; This is a sample Guix Home configuration which can help setup your
 ;; home directory in the same declarative manner as Guix System.
 ;; For more information, see the Home Configuration section of the manual.
-(define-module (guix-home-config)
-  #:use-module (gnu home)
-  #:use-module (gnu home services)
-  #:use-module (gnu home services shells)
-  #:use-module (gnu services)
-  #:use-module (gnu system shadow))
+(use-modules (gnu home)
+             (gnu home services)
+             (gnu home services shells)
+             (gnu packages)
+             (gnu services)
+             (gnu system)
+             (gnu system shadow)
+             (nongnu packages)
+             (gnu packages base)
+             (gnu packages commencement)
+             (nongnu packages clojure)
+             (gnu packages compression)
+             (gnu packages debian)
+             (gnu packages nss)
+             (gnu packages package-management)
+             (gnu packages version-control)
+             ;;(gnu packages xdisorg)           ;; For X packages
+             (gnu packages xorg)
+             (gnu packages web)
+             (gnu packages ssh)
+             (gnu packages shells)
+             (gnu packages gcc)
+             (gnu packages gnome)
+             (gnu packages gtk)
+             (gnu packages gdb)
+             (gnu packages glib)
+             (gnu packages gl)
+             (gnu packages librewolf)
+             (gnu packages linux)
+             (gnu packages sdl)
+             (gnu packages pretty-print)
+             (gnu packages java)
+             (gnu packages databases)
+             (gnu packages valgrind)
+             (gnu packages video)
+             (gnu packages vulkan)
+             (nongnu packages mozilla)
+             (nongnu packages game-client)
+             (nongnu packages editors)
+             (gchannel packages vivaldi)
+             (gchannel packages bolt-launcher)
+             (gchannel packages edge)
+             (guix gexp))
+
 
 (define home-config
   (home-environment
+  (packages
+  (list
+   librewolf
+   steam
+   vscodium
+   git
+   zsh
+   gdk-pixbuf
+   gcc-toolchain
+   leiningen
+   hicolor-icon-theme
+   gtk
+   gtkmm
+   gdb
+   flatpak
+   dpkg
+   valgrind
+   zlib
+   sdl2
+   pipewire
+   openjdk
+   nss
+   libglvnd
+   fmt
+   ffmpeg
+   strace
+   xhost
+   xauth
+   microsoft-edge-stable
+   bolt-launcher 
+  ))
     (services
       (list
         ;; Uncomment the shell you wish to use for your user:
         ;(service home-bash-service-type)
         ;(service home-fish-service-type)
-        ;(service home-zsh-service-type)
+        (service home-zsh-service-type)
 
         (service home-files-service-type
          `((".guile" ,%default-dotguile)
